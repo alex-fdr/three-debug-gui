227d3ac9ef5ef0dc6ccd0338bdb8da212e4f4fff
alex-fdr <alexfv01@gmail.com>
2025-11-20 18:13:56 +0200

    update config formatting

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
+import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
@@ -12,15 +12,13 @@ export default defineConfig({
         },
         rollupOptions: {
             external: [
-                'three', 
+                'three',
                 'three/addons/controls/OrbitControls',
                 'three/addons/controls/TransformControls',
-                'lil-gui'
-            ]
+                'lil-gui',
+            ],
         },
         reportCompressedSize: false,
     },
-    plugins: [
-        dts({ rollupTypes: true })
-    ]
-})
+    plugins: [dts({ rollupTypes: true })],
+});
